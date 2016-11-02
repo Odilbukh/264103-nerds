abcd1810a82feb939fd99efc63f25e3530290c2a
Odilbukh <odil.bukh@mail.ru>
2016-11-02 15:49:49 +0500

    02.11.2016

--- js/script.js
+++ js/script.js
@@ -1,62 +1,41 @@
-var open = document.querySelector(".map-btn");
-var popup = document.querySelector(".feedback");
-var close = popup.querySelector(".feedback-circle-btn");
-var cancel = popup.querySelector(".feedback-cancel");
-var login = popup.querySelector("[name=login]");
-var form = popup.querySelector("form");
-var storage = localStorage.getItem("login");
-
-open.addEventListener("click", function (event) {
-    event.preventDefault(event);
-    popup.classList.add("feedback-show");
-    login.focus();
-    if (storage) {
-        login.value = storage;
-    } else {
-        login.focus();
-    }
-});
-
-close.addEventListener("click", function (event) {
-    event.preventDefault(event);
-    popup.classList.remove("feedback-show");
-});
-cancel.addEventListener("click", function (event) {
-    event.preventDefault(event);
-    popup.classList.remove("feedback-show");
-});
-
-form.addEventListener("submit", function (event) {
-    localStorage.setItem("login", login.value);
-});
-
-window.addEventListener("keydown", function (event) {
-    if (event.keyCode == 27) {
-        if (popup.classList.contains("feedback-show")) {
-            popup.classList.remove("feedback-show");
-        }
-    }
-});
-
+// MAP
 function initialize() {
-    var x = 45.038739;
-    var y = 38.973099;
-    var mapOptions = {
-        zoom: 17,
-        center: new google.maps.LatLng(x, y),
-        scrollwheel: false,
-		disableDefaultUI: true
-    }
-    var map = new  google.maps.Map(
-        document.querySelector(".map-script"),
-        mapOptions
-    );
-    var image = "img/map-marker.png"; 
-    var myLatLng = new google.maps.LatLng(x, y);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
+  var mapOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(59.938707,30.323809)
+  }
+  var map = new google.maps.Map(document.getElementById('map'),
+                                mapOptions);
+  var image = "img/marker.png";
+  var myLatLng = new google.maps.LatLng(59.938707,30.323809);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
 }
-google.maps.event.addDomListener(window, "load", initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
+// END MAP
+
+// POPUP
+var link = document.querySelector(".map-content .btn-red");
+var popup = document.querySelector(".modal-form");
+var cancel = document.querySelector(".modal-form .btn-gray")
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-form-show");
+});
+
+cancel.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-form-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode == 27) {
+    if (popup.classList.contains("modal-form-show")) {
+      popup.classList.remove("modal-form-show");
+    }
+  }
+});
